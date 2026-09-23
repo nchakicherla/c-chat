300564785bdce717c8d739be920ab39cb798b61f
nchakicherla <nchaki6@icloud.com>
2026-09-23 14:43:38 -0700

    add error handling

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
 
@@ -6,9 +9,19 @@ int main(void) {
 	printf("libcurl version: %s\n", curl_version());
 
 	cJSON *json = cJSON_CreateObject();
-	cJSON_AddStringToObject(json, "message", "hello, world");
+	if (!json) {
+		fprintf(stderr, "[ERROR] failed to create JSON object\n");
+		return 1;
+	}
+	if (!cJSON_AddStringToObject(json, "message", "hello, world")) {
+		fprintf(stderr, "[ERROR] failed to add string to JSON object\n");
+		return 1;
+	}
 
 	char* jsontext = cJSON_Print(json);
+	if (!jsontext) {
+		fprintf(stderr, "[ERROR] failed to create print buffer for JSON object\n");
+	}
 	printf("json text: \n%s\n", jsontext);
 	cJSON_free(jsontext);
 
@@ -26,8 +39,8 @@ int main(void) {
 	CURLcode result = curl_easy_perform(curl);
 
 	if (result != CURLE_OK) {
-		fprintf(stderr, "[ERROR] curl error: %s\n", 
-			curl_easy_strerror(result));
+		fprintf(stderr, "[ERROR] curl error: %s\n", curl_easy_strerror(result));
+		curl_easy_cleanup(curl);
 		return 1;
 	}
 
